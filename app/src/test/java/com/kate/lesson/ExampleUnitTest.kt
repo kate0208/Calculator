d81495099e74a0dc87fbd5fe6cf52647bc3586de
Kate <a0921188994@gmail.com>
2022-02-03 13:47:46 +0800

    rename project name and application id

--- app/src/test/java/com/kate/lesson/ExampleUnitTest.kt
+++ app/src/test/java/com/kate/lesson/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
-package com.kate.calculator
+package com.kate.lesson
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
