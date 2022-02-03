8e3e30f376d6bb47611f4385fead0841b14a7402
Kate <a0921188994@gmail.com>
2022-02-03 23:04:26 +0800

    finish lesson1-1

--- app/src/main/java/com/kate/lesson/MainActivity.kt
+++ app/src/main/java/com/kate/lesson/MainActivity.kt
@@ -1,12 +1,35 @@
 package com.kate.lesson
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+  private lateinit var editText: EditText
+  private lateinit var kmTextView: TextView
+  private lateinit var convertButton: Button
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
-    // hello world
+
+    editText = findViewById<EditText>(R.id.editText)
+    kmTextView = findViewById<TextView>(R.id.kmTextView)
+    convertButton = findViewById<Button>(R.id.convertButton)
+
+    convertButton.setOnClickListener {
+      val miles = editText.text.toString().toDoubleOrNull()
+
+      if (miles != null) {
+        val km = (miles * 1.61).toString()
+        kmTextView.text = km
+      } else {
+        kmTextView.text = getString(R.string.plz)
+      }
+
+    }
   }
 }
