231277de3d46db1f821e2b151b589d5635c7e1cb
Kate <a0921188994@gmail.com>
2022-02-06 17:22:24 +0800

    finish1-2

--- app/src/main/java/com/kate/lesson/MainActivity.kt
+++ app/src/main/java/com/kate/lesson/MainActivity.kt
@@ -2,22 +2,23 @@ package com.kate.lesson
 
 import android.os.Bundle
 import android.view.View
-import android.widget.TextView
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.kate.lesson.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-  private lateinit var binding: ActivityMainBinding
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
+  private lateinit var binding: ActivityMainBinding
 
   //new一個interface出來
   //object : interface name (點setOnClickListener進去看--public interface "OnClickListener")
-  private val onClickListener = object : View.OnClickListener {
-    override fun onClick(v: View) {
-      println((v as TextView).text)
-    }
-
-  }
+//  private val onClickListener = object : View.OnClickListener {
+//    override fun onClick(v: View) {
+//
+//      binding.phoneNumber.text="${binding.phoneNumber.text}${(v as Button).text}"
+//    }
+//
+//  }
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -25,13 +26,20 @@ class MainActivity : AppCompatActivity() {
     val view = binding.root
     setContentView(view)
 
-    binding.one.setOnClickListener(onClickListener)
-    binding.two.setOnClickListener(onClickListener)
-    binding.three.setOnClickListener(onClickListener)
-    binding.four.setOnClickListener(onClickListener)
-    binding.five.setOnClickListener(onClickListener)
-    binding.six.setOnClickListener(onClickListener)
+    binding.one.setOnClickListener(this)
+    binding.two.setOnClickListener(this)
+    binding.three.setOnClickListener(this)
+    binding.four.setOnClickListener(this)
+    binding.five.setOnClickListener(this)
+    binding.six.setOnClickListener(this)
 
+    binding.clear.setOnClickListener {
+      binding.phoneNumber.text = ""
+    }
 
+  }
+
+  override fun onClick(v: View?) {
+    binding.phoneNumber.text = "${binding.phoneNumber.text}${(v as Button).text}"
   }
 }
