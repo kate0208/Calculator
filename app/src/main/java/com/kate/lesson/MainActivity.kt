d933be5ecdc6e14ba11af354e40e985dffe47880
Kate <a0921188994@gmail.com>
2022-02-09 22:54:48 +0800

    toast

--- app/src/main/java/com/kate/lesson/MainActivity.kt
+++ app/src/main/java/com/kate/lesson/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.kate.lesson
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.kate.lesson.databinding.ActivityMainBinding
 
@@ -12,6 +13,9 @@ class MainActivity : AppCompatActivity() {
     binding = ActivityMainBinding.inflate(layoutInflater)
     val view = binding.root
     setContentView(view)
+    binding.helloWorld.setOnClickListener {
+      Toast.makeText(this, getString(R.string.toast_text), Toast.LENGTH_SHORT).show()
 
+    }
   }
 }
