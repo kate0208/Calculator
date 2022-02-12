49161fcc2b85eb255a0b5db38893c0f569236a55
Kate <a0921188994@gmail.com>
2022-02-12 16:35:01 +0800

    dialog

--- app/src/main/java/com/kate/lesson/MainActivity.kt
+++ app/src/main/java/com/kate/lesson/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.kate.lesson
 
+import android.content.DialogInterface
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.kate.lesson.databinding.ActivityMainBinding
 
@@ -12,6 +15,17 @@ class MainActivity : AppCompatActivity() {
     binding = ActivityMainBinding.inflate(layoutInflater)
     val view = binding.root
     setContentView(view)
-
+    binding.helloWorld.setOnClickListener {
+      AlertDialog.Builder(this)
+        .setMessage("Your BMI is 1234")
+        .setTitle("BMI")
+        .setPositiveButton("OK", object :DialogInterface.OnClickListener{
+          override fun onClick(dialog: DialogInterface?, which: Int) {
+            Toast.makeText(this@MainActivity, "Hello", Toast.LENGTH_LONG).show()
+          }
+        })
+        .setNegativeButton("Cancel", null)
+        .show()
+    }
   }
 }
