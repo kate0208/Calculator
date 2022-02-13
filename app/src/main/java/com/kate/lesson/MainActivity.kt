83917e001e5afe54449ce0e6e1ad75a35cd6fdf7
Kate <a0921188994@gmail.com>
2022-02-13 22:40:42 +0800

    radio

--- app/src/main/java/com/kate/lesson/MainActivity.kt
+++ app/src/main/java/com/kate/lesson/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.kate.lesson
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.kate.lesson.databinding.ActivityMainBinding
 
@@ -12,6 +13,23 @@ class MainActivity : AppCompatActivity() {
     binding = ActivityMainBinding.inflate(layoutInflater)
     val view = binding.root
     setContentView(view)
+    binding.female.isChecked = true
+    binding.pickGender.setOnClickListener {
+      showGender()
+    }
+  }
+
+  private fun showGender() {
+//    if (binding.female.isChecked) {
+//      Toast.makeText(this, "你選擇了美女", Toast.LENGTH_SHORT).show()
+//    } else if (binding.male.isChecked) {
+//      Toast.makeText(this, "你選擇了宅男", Toast.LENGTH_SHORT).show()
+//
+//    }
+    when {
+      binding.female.isChecked -> Toast.makeText(this, "你選擇了美女", Toast.LENGTH_SHORT).show()
+      binding.male.isChecked -> Toast.makeText(this, "你選擇了宅男", Toast.LENGTH_SHORT).show()
+    }
 
   }
 }
