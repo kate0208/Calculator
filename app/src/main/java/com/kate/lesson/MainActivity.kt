22cea5606491cdd82e559b26a832ba777fab58e2
Kate <a0921188994@gmail.com>
2022-02-17 23:07:30 +0800

    SharedPreferences

--- app/src/main/java/com/kate/lesson/MainActivity.kt
+++ app/src/main/java/com/kate/lesson/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.kate.lesson
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.kate.lesson.databinding.ActivityMainBinding
 
@@ -13,13 +12,29 @@ class MainActivity : AppCompatActivity() {
     binding = ActivityMainBinding.inflate(layoutInflater)
     val view = binding.root
     setContentView(view)
-    binding.switchCompat.setOnCheckedChangeListener { buttonView, isChecked /*isChecked 是參數 */ ->
-      if (isChecked) {
-        Toast.makeText(this, getString(R.string.switch_checked), Toast.LENGTH_SHORT).show()
-      } else {
-        Toast.makeText(this, getString(R.string.switch_not_checked), Toast.LENGTH_SHORT).show()
 
-      }
+    readSwitchData()
+
+    binding.switchCompat.setOnCheckedChangeListener { buttonView, isChecked /*isChecked 是參數 */ ->
+      saveSwitchData()
     }
+  }
+
+  private fun readSwitchData() {
+    val settings = getSharedPreferences(DATA, 0)
+    val isChecked = settings.getBoolean(SWITCH, false)
+    binding.switchCompat.isChecked = isChecked
+  }
+
+  private fun saveSwitchData() {
+    val settings = getSharedPreferences(DATA, 0)
+    settings.edit()
+      .putBoolean(SWITCH, binding.switchCompat.isChecked)
+      .apply()
+  }
+
+  companion object {
+    private const val DATA = "DATA"
+    private const val SWITCH = "SWITCH"
   }
 }
