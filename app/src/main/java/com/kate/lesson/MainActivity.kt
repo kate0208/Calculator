9ad8763113338148f5f40628a07b43a7d0ee7d88
Kate <a0921188994@gmail.com>
2022-02-21 21:35:57 +0800

    add glide library

--- app/src/main/java/com/kate/lesson/MainActivity.kt
+++ app/src/main/java/com/kate/lesson/MainActivity.kt
@@ -2,6 +2,7 @@ package com.kate.lesson
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.kate.lesson.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,5 +14,13 @@ class MainActivity : AppCompatActivity() {
     val view = binding.root
     setContentView(view)
 
+    Glide.with(this)
+      .load(getProfilePictureUrl())
+      .into(binding.helloWorld)
+
+  }
+
+  private fun getProfilePictureUrl(): String {
+    return "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F28%2F2020%2F10%2F13%2Fcorgi-dog-POPDOGNAME1020.jpg"
   }
 }
