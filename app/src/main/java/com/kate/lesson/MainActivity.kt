17ccdac6ef84280d4086588fcc77f0c45a123785
Kate <a0921188994@gmail.com>
2022-05-04 20:50:05 +0800

    add recyclerview

--- app/src/main/java/com/kate/lesson/MainActivity.kt
+++ app/src/main/java/com/kate/lesson/MainActivity.kt
@@ -2,9 +2,12 @@ package com.kate.lesson
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.kate.lesson.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
   private lateinit var binding: ActivityMainBinding
 
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,5 +16,17 @@ class MainActivity : AppCompatActivity() {
     val view = binding.root
     setContentView(view)
 
+    val adapter = MainAdapter()
+
+    binding.recyclerView.layoutManager = LinearLayoutManager(this)
+    binding.recyclerView.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
+    binding.recyclerView.adapter = adapter
+
+    val list = mutableListOf<String>()
+    for (i in 0 until 50) {
+      list.add(i.toString())
+    }
+    adapter.submitList(list)
   }
+
 }
