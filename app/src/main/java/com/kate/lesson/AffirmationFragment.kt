86ace0bf644778445e3a425eaca1ae033fdd6303
Kate <a0921188994@gmail.com>
2022-05-22 15:31:32 +0800

    completed mvc

--- app/src/main/java/com/kate/lesson/AffirmationFragment.kt
+++ app/src/main/java/com/kate/lesson/AffirmationFragment.kt
@@ -1,20 +1,23 @@
 package com.kate.lesson
 
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.kate.lesson.databinding.FragmentAffirmationBinding
 
-class AffirmationFragment: Fragment() {
+class AffirmationFragment : Fragment() {
 
   private var _binding: FragmentAffirmationBinding? = null
+
   // This property is only valid between onCreateView and
   // onDestroyView.
   private val binding get() = _binding!!
+
+  private val model = MainModel()
 
   override fun onCreateView(
     inflater: LayoutInflater,
@@ -26,6 +29,7 @@ class AffirmationFragment: Fragment() {
     return view
   }
 
+
   override fun onDestroyView() {
     super.onDestroyView()
     _binding = null
@@ -34,57 +38,16 @@ class AffirmationFragment: Fragment() {
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
 
-    val list = affirmationList(requireContext())
+    val list = model.affirmationList(requireContext())
     val adapter = MainAdapter()
     binding.recyclerView.layoutManager = LinearLayoutManager(requireContext())
     binding.recyclerView.adapter = adapter
 
     adapter.submitList(list)
-  }
 
-  private fun affirmationList(context: Context): List<Affirmation> {
-    return listOf(
-      Affirmation(
-        title = context.getString(R.string.affirmation1),
-        imageResourceId = R.drawable.image1
-      ),
-      Affirmation(
-        title = context.getString(R.string.affirmation2),
-        imageResourceId = R.drawable.image2
-      ),
-      Affirmation(
-        title = context.getString(R.string.affirmation3),
-        imageResourceId = R.drawable.image3
-      ),
-      Affirmation(
-        title = context.getString(R.string.affirmation4),
-        imageResourceId = R.drawable.image4
-      ),
-      Affirmation(
-        title = context.getString(R.string.affirmation5),
-        imageResourceId = R.drawable.image5
-      ),
-      Affirmation(
-        title = context.getString(R.string.affirmation6),
-        imageResourceId = R.drawable.image6
-      ),
-      Affirmation(
-        title = context.getString(R.string.affirmation7),
-        imageResourceId = R.drawable.image7
-      ),
-      Affirmation(
-        title = context.getString(R.string.affirmation8),
-        imageResourceId = R.drawable.image8
-      ),
-      Affirmation(
-        title = context.getString(R.string.affirmation9),
-        imageResourceId = R.drawable.image9
-      ),
-      Affirmation(
-        title = context.getString(R.string.affirmation10),
-        imageResourceId = R.drawable.image10
-      ),
-    )
+    binding.nextButton.setOnClickListener {
+      view.findNavController().navigate(AffirmationFragmentDirections.actionAffirmationFragmentToDetailFragment())
+    }
   }
 
 
---
+++ app/src/main/java/com/kate/lesson/DetailFragment.kt
@@ -0,0 +1,35 @@
+package com.kate.lesson
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.kate.lesson.databinding.FragmentDetailBinding
+
+/**
+ * Created by Kate on 2022/5/20.
+ */
+class DetailFragment : Fragment() {
+  private var _binding: FragmentDetailBinding? = null
+
+  // This property is only valid between onCreateView and
+  // onDestroyView.
+  private val binding get() = _binding!!
+
+  override fun onCreateView(
+    inflater: LayoutInflater,
+    container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View? {
+    _binding = FragmentDetailBinding.inflate(inflater, container, false)
+    val view = binding.root
+    return view
+  }
+
+  override fun onDestroyView() {
+    super.onDestroyView()
+    _binding = null
+  }
+
+}
