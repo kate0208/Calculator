90fd48a2e5bf525134ae6f064355444e4bfff67b
Kate <a0921188994@gmail.com>
2022-06-12 14:34:02 +0800

    setup start code

---
+++ app/src/main/java/com/kate/lesson/overview/OverviewViewModel.kt
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2018, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.kate.lesson.overview
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+/**
+ * The [ViewModel] that is attached to the [OverviewFragment].
+ */
+class OverviewViewModel : ViewModel() {
+
+  val response = MutableLiveData<String>()
+
+  /**
+   * Call getMarsRealEstateProperties() on init so we can display status immediately.
+   */
+  init {
+    getMarsRealEstateProperties()
+  }
+
+  /**
+   * Sets the value of the status LiveData to the Mars API status.
+   */
+  private fun getMarsRealEstateProperties() {
+    response.value = "Set the Mars API Response here!"
+  }
+}
