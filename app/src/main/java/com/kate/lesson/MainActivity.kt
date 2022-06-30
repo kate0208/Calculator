ac266b079527108997976d79f502137c1c00960b
Kate <a0921188994@gmail.com>
2022-06-30 21:20:02 +0800

    refactor

--- app/src/main/java/com/kate/lesson/MainActivity.kt
+++ app/src/main/java/com/kate/lesson/MainActivity.kt
@@ -1,32 +1,43 @@
 package com.kate.lesson
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.appcompat.app.AppCompatActivity
 import com.kate.lesson.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
   private lateinit var binding: ActivityMainBinding
+  private var number = 0
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+
     binding = ActivityMainBinding.inflate(layoutInflater)
     val view = binding.root
     setContentView(view)
 
-    binding.plus.setOnClickListener {
-      var number = binding.number.text.toString().toIntOrNull()
-      if (number != null) {
-        number += 1
-        binding.number.setText(number.toString())
+
+    binding.number.addTextChangedListener(object : TextWatcher {
+      override fun afterTextChanged(s: Editable?) {
       }
+
+      override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+      }
+
+      override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+        number = s.toString().toIntOrNull() ?: 0
+      }
+    })
+
+    binding.plus.setOnClickListener {
+      number += 1
+      binding.number.setText(number.toString())
     }
 
     binding.minus.setOnClickListener {
-      var number = binding.number.text.toString().toIntOrNull()
-      if (number != null) {
-        number -= 1
-        binding.number.setText(number.toString())
-      }
+      number -= 1
+      binding.number.setText(number.toString())
     }
 
   }
