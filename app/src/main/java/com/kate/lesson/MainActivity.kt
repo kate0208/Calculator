08c494770940e6900e47d60359fd886850d6a875
Kate <a0921188994@gmail.com>
2022-07-03 16:07:49 +0800

    refactor

--- app/src/main/java/com/kate/lesson/MainActivity.kt
+++ app/src/main/java/com/kate/lesson/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.kate.lesson.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
   private lateinit var binding: ActivityMainBinding
 
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,10 +14,31 @@ class MainActivity : AppCompatActivity() {
     binding = ActivityMainBinding.inflate(layoutInflater)
     val view = binding.root
     setContentView(view)
-    binding.female.isChecked = true
+
+    readSwitchData()
+//    binding.female.isChecked = true
     binding.pickGender.setOnClickListener {
       showGender()
     }
+  }
+
+  private fun readSwitchData() {
+    val settings = getSharedPreferences(DATA, 0)
+    binding.male.isChecked = settings.getBoolean(MALE, false)
+    binding.female.isChecked = settings.getBoolean(FEMALE, false)
+  }
+
+  private fun saveSwitchData() {
+    val settings = getSharedPreferences(DATA, 0)
+    settings.edit()
+      .putBoolean(MALE, binding.male.isChecked)
+      .putBoolean(FEMALE, binding.female.isChecked)
+      .apply()
+  }
+  companion object {
+    private const val DATA = "DATA"
+    private const val MALE = "MALE"
+    private const val FEMALE = "FEMALE"
   }
 
   private fun showGender() {
@@ -30,6 +52,7 @@ class MainActivity : AppCompatActivity() {
       binding.female.isChecked -> Toast.makeText(this, "你選擇了美女", Toast.LENGTH_SHORT).show()
       binding.male.isChecked -> Toast.makeText(this, "你選擇了宅男", Toast.LENGTH_SHORT).show()
     }
+    saveSwitchData()
 
   }
 }
