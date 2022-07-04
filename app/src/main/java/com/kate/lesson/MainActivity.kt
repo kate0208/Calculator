080b12ba15b494a58df9b999392db4e3c815d6ac
Kate <a0921188994@gmail.com>
2022-07-04 22:06:00 +0800

    add SharedPreferences

--- app/src/main/java/com/kate/lesson/MainActivity.kt
+++ app/src/main/java/com/kate/lesson/MainActivity.kt
@@ -15,12 +15,32 @@ class MainActivity : AppCompatActivity() {
     binding = ActivityMainBinding.inflate(layoutInflater)
     val view = binding.root
     setContentView(view)
-
+    readSwitchData()
     binding.enter.setOnClickListener {
       number += 1
       binding.number.text = number.toString()
+      saveSwitchData()
     }
 
+  }
+
+  private fun readSwitchData() {
+    val settings = getSharedPreferences(DATA, 0)
+    val number = settings.getString(NUMBER, "0")
+    binding.number.text = number
+    this.number = number?.toIntOrNull() ?: 0   //assign 第11行的值   打this是因為有兩個名字一樣 指的是class裡的number
+  }
+
+  private fun saveSwitchData() {
+    val settings = getSharedPreferences(DATA, 0)
+    settings.edit()
+      .putString(NUMBER, binding.number.text.toString())
+      .apply()
+  }
+
+  companion object {
+    private const val DATA = "DATA"
+    private const val NUMBER = "NUMBER"
   }
 
 }
