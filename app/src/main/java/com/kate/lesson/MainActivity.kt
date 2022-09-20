5676464fdf763c72b47709b50e58e985108adb16
Kate <a0921188994@gmail.com>
2022-09-20 16:15:44 +0800

    finish demo

--- app/src/main/java/com/kate/lesson/MainActivity.kt
+++ app/src/main/java/com/kate/lesson/MainActivity.kt
@@ -13,5 +13,20 @@ class MainActivity : AppCompatActivity() {
     val view = binding.root
     setContentView(view)
 
+    binding.calculate.setOnClickListener {
+      val tall = binding.editTall.text.toString().toDoubleOrNull() ?: return@setOnClickListener
+      val weight = binding.editWeight.text.toString().toDoubleOrNull() ?: return@setOnClickListener
+      val age = binding.editAge.text.toString().toDoubleOrNull() ?: return@setOnClickListener
+      val bmi = weight / ((tall / 100) * (tall / 100))
+      binding.tvBmi.text = "BMI \n${String.format("%.2f", bmi)}"
+
+      if (binding.boy.isChecked) {
+        binding.tvWeight.text = "標準體重 \n${String.format("%.2f", ((tall - 80) * 0.7))}"
+        binding.tvFat.text = "體脂肪 \n${String.format("%.2f", (bmi * 1.39 + age * 0.16 - 19.34))}"
+      } else {
+        binding.tvWeight.text = "標準體重\n" + String.format("%.2f", ((tall - 70) * 0.6))
+        binding.tvFat.text = "體脂肪\n" + String.format("%.2f", (bmi * 1.39 + age * 0.16 - 9))
+      }
+    }
   }
 }
