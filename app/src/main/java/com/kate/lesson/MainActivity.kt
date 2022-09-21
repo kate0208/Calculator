43d0bf82b06a675c2a58ddbc619a45b158f0abda
Kate <a0921188994@gmail.com>
2022-09-21 14:33:46 +0800

    review dialog

--- app/src/main/java/com/kate/lesson/MainActivity.kt
+++ app/src/main/java/com/kate/lesson/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.kate.lesson
 
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.kate.lesson.databinding.ActivityMainBinding
 
@@ -12,6 +14,20 @@ class MainActivity : AppCompatActivity() {
     binding = ActivityMainBinding.inflate(layoutInflater)
     val view = binding.root
     setContentView(view)
+
+    val item = arrayOf("麥當勞", "肯德基", "鹹酥雞", "牛肉麵", "滷肉飯")
+
+    binding.food.setOnClickListener {
+      AlertDialog.Builder(this)
+        .setTitle("今天你想吃什麼")
+        .setItems(item) { DialogInterface, i ->
+          Toast.makeText(this, "今天想吃${item[i]}", Toast.LENGTH_SHORT).show()
+        }
+        .setNeutralButton("減肥不吃好了", { DialogInterface, i ->
+          Toast.makeText(this, "不吃拉哼！", Toast.LENGTH_SHORT).show()
+        })
+        .show()
+    }
 
   }
 }
